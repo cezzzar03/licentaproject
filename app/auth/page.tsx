ae2efafc64b1c747c76f04985ec1bebcabdbdfea
cezzzar03 <148795391+cezzzar03@users.noreply.github.com>
2025-03-27 14:28:24 +0200

    Setup refacut de la 0. Putem incepe autentificare page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,3 +1,7 @@
-export default function AuthPage() {
-  return <h1>Pagina de autentificare (Login + Register)</h1>;
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-900 text-white">
+      <h1 className="text-4xl font-bold">Tailwind e ON 🔥</h1>
+    </div>
+  );
 }
