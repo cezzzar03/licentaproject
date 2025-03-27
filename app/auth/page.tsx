5d34d29f389e514b4c5e882c608fe5a36e7adffa
cezzzar03 <148795391+cezzzar03@users.noreply.github.com>
2025-03-27 14:52:21 +0200

    Am adaugat imagine de fundal pe pagina de autentificare

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-900 text-white">
-      <h1 className="text-4xl font-bold">Tailwind e ON 🔥</h1>
+    <div
+      className="min-h-screen flex items-center justify-center text-white bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/CryptoBackground.png')" }}
+    >
     </div>
   );
 }
