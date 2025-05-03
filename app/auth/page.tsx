ed9b054738a0f41e4ec7097489597555bc3d257a
cezzzar03 <148795391+cezzzar03@users.noreply.github.com>
2025-05-03 22:17:51 +0300

    Acum e defapt gata partea stanga a auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
       <div className="w-full max-w-3xl bg-white rounded-xl p-0 shadow-lg flex overflow-hidden h-[500px]">
           
           <div className="w-1/2 bg-gradient-to-r from-slate-900 to-blue-900 text-white p-8 min-h-[400px] flex flex-col">
-                <h2 className="text-6xl font-bold text-white drop-shadow-[0_0_10px_rgba(255,255,255,0.6)] mb-20">CryptoHub</h2>
+                <h2 className="text-6xl font-bold mb-20 text-white transition duration-300 hover:text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">CryptoHub</h2>
                 <p className="text-base text-gray-200 leading-relaxed">
                    CryptoHub este locul ideal pentru a învăța, experimenta și înțelege ecosistemul criptomonedelor. 
                 </p>
                 <div className="mt-auto flex justify-center">
-                  <img src="/images/logo.png" alt="CH Logo" className="w-16 h-16" />
+                  <img src="/images/logo.png" alt="CH Logo" className="w-16 h-16 transition duration-300 hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]" />
                 </div>
           </div>
 
