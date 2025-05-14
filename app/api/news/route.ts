3b3fbe05f1194484524d45e4a0a544fbd037f422
cezzzar03 <148795391+cezzzar03@users.noreply.github.com>
2025-05-14 22:04:54 +0300

    am adaugat stiri random

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -2,10 +2,14 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
+    const topics = ['crypto', 'bitcoin', 'blockchain'];
+    const q = topics[Math.floor(Math.random() * topics.length)];
+
     const res = await fetch(
-      `https://gnews.io/api/v4/search?q=crypto&lang=en&max=5&token=${process.env.GNEWS_API_KEY}`
+      `https://gnews.io/api/v4/search?q=${q}&lang=en&max=5&sortby=publishedAt&token=${process.env.GNEWS_API_KEY}`
     );
     const data = await res.json();
+
     return NextResponse.json(data);
   } catch (err) {
     console.error("Eroare GNews:", err);
