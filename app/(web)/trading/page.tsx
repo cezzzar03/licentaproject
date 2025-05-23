c149e3a9c2567fa5bd2a90aca084d2806edd76eb
cezzzar03 <148795391+cezzzar03@users.noreply.github.com>
2025-05-23 17:05:51 +0300

    Putem incepe ultima parte

--- app/(web)/trading/page.tsx
+++ app/(web)/trading/page.tsx
@@ -2,8 +2,21 @@
 
 export default function TradingPage() {
   return (
-    <div className="max-w-2xl mx-auto p-6 text-center">
-      <h1 className="text-3xl font-bold text-black-700">🎉 Bine ai venit în Ghidul de Trading!</h1>
+    <div className="p-6 space-y-12 bg-white rounded-lg shadow-md max-w-3xl mx-auto">
+    
+      <div className="max-w-2xl mx-auto p-6 text-center">
+        <h1 className="text-3xl font-bold text-black-700">🎉 Bine ai venit în Ghidul de Trading!</h1>
+      </div>
+
+      <div className="space-y-10 text-gray-800 text-left">
+        <section>
+          <h2 className="text-2xl font-semibold">1. Ce este tradingul?</h2>
+          <p>
+            PROBĂ
+          </p>
+        </section>
+      </div>
+      
     </div>
   );
 }
